56dfe9fa64834d45ed7de3f830b437f3bde1fadf
Asad Gulzar <asadgulxar10@gmail.com>
2025-04-08 14:45:50 +0500

    Spiral matrix

--- matrix.js
+++ matrix.js
@@ -19,3 +19,47 @@ var rotate = function (matrix) {
     matrix[i].reverse();
   }
 };
+
+// Given an m x n matrix, return all elements of the matrix in spiral order.
+
+// https://leetcode.com/problems/spiral-matrix/description/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function (matrix) {
+  let top = 0;
+  let left = 0;
+  let bottom = matrix.length - 1;
+  let right = matrix[0].length - 1;
+  const result = [];
+
+  while (top <= bottom && left <= right) {
+    for (let i = left; i <= right; i++) {
+      result.push(matrix[top][i]);
+    }
+    top++;
+
+    for (let i = top; i <= bottom; i++) {
+      result.push(matrix[i][right]);
+    }
+    right--;
+
+    if (top <= bottom) {
+      for (let i = right; i >= left; i--) {
+        result.push(matrix[bottom][i]);
+      }
+      bottom--;
+    }
+
+    if (left <= right) {
+      for (let i = bottom; i >= top; i--) {
+        result.push(matrix[i][left]);
+      }
+      left++;
+    }
+  }
+
+  return result;
+};
