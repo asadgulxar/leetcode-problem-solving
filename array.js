9fb518f22c091eeb64972d6c80823cf56dcf575f
Asad Gulzar <asadgulxar10@gmail.com>
2025-04-09 16:28:30 +0500

    sorted array to BST

--- array.js
+++ array.js
@@ -193,3 +193,34 @@ var longestConsecutive = function (nums) {
 
   return longestStreak;
 };
+
+// 108. Convert Sorted Array to Binary Search Tree
+// Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  function buildTree(left, right) {
+    if (left > right) return null;
+
+    const mid = Math.floor((left + right) / 2);
+
+    const root = new TreeNode(nums[mid]);
+
+    root.left = buildTree(left, mid - 1);
+    root.right = buildTree(mid + 1, right);
+    return root;
+  }
+
+  return buildTree(0, nums.length - 1);
+};
