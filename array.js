2df7409326b82bb05f075d14d1acd8c8cb5a8473
Asad Gulzar <asadgulxar10@gmail.com>
2025-04-09 14:41:39 +0500

    longest consecutive sequence

--- array.js
+++ array.js
@@ -156,3 +156,40 @@ var containsNearbyDuplicate = function (nums, k) {
 
   return false;
 };
+
+// 128. Longest Consecutive Sequence
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// You must write an algorithm that runs in O(n) time.
+
+// Example 1:
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {
+  const numSet = new Set(nums);
+  let longestStreak = 0;
+
+  for (const num of numSet) {
+    // Only start counting if it's the beginning of a sequence
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum += 1;
+        currentStreak += 1;
+      }
+
+      longestStreak = Math.max(longestStreak, currentStreak);
+    }
+  }
+
+  return longestStreak;
+};
