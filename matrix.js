dfca7402226258a3e9f8b8d7e60f0543243eb3fb
Asad Gulzar <asadgulxar10@gmail.com>
2025-04-09 22:11:49 +0500

    binary search 2D array

--- matrix.js
+++ matrix.js
@@ -63,3 +63,45 @@ var spiralOrder = function (matrix) {
 
   return result;
 };
+
+// 74. Search a 2D Matrix
+
+// You are given an m x n integer matrix matrix with the following two properties:
+
+// Each row is sorted in non-decreasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+
+// You must write a solution in O(log(m * n)) time complexity.
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  if (!matrix || matrix.length === 0 || matrix[0].length === 0) {
+    return false;
+  }
+
+  const m = matrix.length;
+  const n = matrix[0].length;
+
+  let left = 0,
+    right = m * n - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midValue = matrix[Math.floor(mid / n)][mid % n];
+
+    if (midValue === target) {
+      return true;
+    } else if (midValue < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return false;
+};
