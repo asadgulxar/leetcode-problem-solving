e7962e910397ba2d1c9ab86f34f0efffc10732bb
Asad Gulzar <asadgulxar10@gmail.com>
2025-04-10 11:12:00 +0500

    solved Matrix min path sum problem

--- array.js
+++ array.js
@@ -285,3 +285,30 @@ var sortList = function (head) {
   let right = sortList(mid);
   return merge(left, right);
 };
+
+// 64. Minimum Path Sum
+// Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+// Note: You can only move either down or right at any point in time.
+
+// https://leetcode.com/problems/minimum-path-sum/description/
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function (grid) {
+  const m = grid.length;
+  const n = grid[0].length;
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === 0 && j === 0) continue;
+      else if (i === 0) grid[i][j] += grid[i][j - 1];
+      else if (j === 0) grid[i][j] += grid[i - 1][j];
+      else grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+    }
+  }
+
+  return grid[m - 1][n - 1];
+};
