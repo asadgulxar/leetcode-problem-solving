49efc9aade9fd048014f503812c186520d9e8e51
Asad Gulzar <asadgulxar10@gmail.com>
2025-04-15 12:58:49 +0500

    climibing stairs problem

--- array.js
+++ array.js
@@ -399,3 +399,28 @@ var hammingWeight = function (n) {
   }
   return count;
 };
+
+// 70. Climbing Stairs
+
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  if (n <= 1) return 1;
+
+  let a = 1; // f(0)
+  let b = 1; // f(1)
+
+  for (let i = 2; i <= n; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+
+  return b;
+};
