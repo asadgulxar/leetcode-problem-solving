91344b6ac883ba330965b12c84fad7cad116cf16
Asad Gulzar <asadgulxar10@gmail.com>
2025-04-20 09:14:02 +0500

    reverse linkedlist

--- array.js
+++ array.js
@@ -424,3 +424,30 @@ var climbStairs = function (n) {
 
   return b;
 };
+
+// 206. Reverse Linked List
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null;
+  let current = head;
+  while (current !== null) {
+    let temp = current.next;
+    current.next = prev;
+    prev = current;
+    current = temp;
+  }
+
+  return prev;
+};
