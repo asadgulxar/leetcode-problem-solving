995c25b8afee6e62d599f400d941d648bf9d02c2
Asad Gulzar <asadgulxar10@gmail.com>
2025-04-27 17:58:58 +0500

    Zigzag Conversion solution

--- string.js
+++ string.js
@@ -28,3 +28,40 @@ var isValid = function (s) {
   }
   return !stack.length;
 };
+
+// 6. Zigzag Conversion
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+// And then read line by line: "PAHNAPLSIIGYIR"
+
+// Write the code that will take a string and make this conversion given a number of rows:
+
+// string convert(string s, int numRows);
+
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+  const rows = [];
+  let rowOp = "+";
+  let rowIndex = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (rowOp === "+") {
+      rows[rowIndex] = (rows[rowIndex] || "") + s[i];
+      rowIndex++;
+      if (rowIndex === numRows - 1) rowOp = "-";
+      continue;
+    }
+    if (rowOp === "-") {
+      rows[rowIndex] = (rows[rowIndex] || "") + s[i];
+      rowIndex--;
+      if (rowIndex === 0) rowOp = "+";
+    }
+  }
+  return rows.join("");
+};
